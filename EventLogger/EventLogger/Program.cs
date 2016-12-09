43da848dafaa6d71f88be0a299248f6cceb7be6c
Hongxin-Sid <a_rei02@Live.cn>
2016-12-10 00:50:54 +0800

    File Output Completed
    add more method(ChangeFilePath,etc)

--- EventLogger/EventLogger/Program.cs
+++ EventLogger/EventLogger/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,33 +11,32 @@ namespace EventLogger
 {
     class EventLogger
     {
-        public readonly DateTime startTime;
-        public bool isUDPOn;
-        public bool isLogging = false;
+        private bool isUdpEnabled = false;
+        private bool isFileEnabled = false;
         
-        public string filePath
+        public string FilePath
         {
             get
             {
-                return _filePath;
+                return filePath;
             }
 
             set
             {
-                _filePath = value;
+                filePath = value;
             }
         }
 
-        private string _filePath;
+        private string filePath;
 
-        public IPEndPoint IpEndPoint
+        public IPEndPoint IPEndPoint
         {
             get
             {
                 return ipEndPoint;
             }
 
-            set
+            private set
             {
                 ipEndPoint = value;
             }
@@ -44,9 +44,11 @@ namespace EventLogger
 
         private IPEndPoint ipEndPoint;
 
+        private UdpClient udpClient;
+
         public EventLogger()
         {
-            startTime = DateTime.Now;
+            Console.WriteLine("New EventLogger ……");
         }
 
         public EventLogger(string filePath):this()
@@ -65,50 +67,62 @@ namespace EventLogger
             this.ipEndPoint = ipEndPoint;
         }
 
-        public void Start()
-        {
-            start(false);
-        }
-
-        public void start(bool fileIsUsed)
+        public void start()
         {
             if (ipEndPoint != null)
             {
-                isUDPOn = true;
+                isUdpEnabled = true;
+                udpClient = new UdpClient(ipEndPoint);
             }
             if (filePath != null)
             {
-                isLogging = true;
-                if (fileIsUsed != true)
-                {
-                    File.Create(filePath);
-                }
+                isFileEnabled = true;
+                //File.Create(filePath);
             }
+            Console.WriteLine("Start Logging ……");
         }
+
         public string log(string errorInfo)
         {
-            string errorMessage = log("", errorInfo);
-            return errorMessage;
+            return log("", errorInfo);
         }
+
         public string log(string errorSource,string errorInfo)
         {
-            DateTime errorTime = DateTime.Now;
-            string errorMessage = errorTime + "\t" + errorInfo;
-            if (isUDPOn == true)
+            string errorMessage;
+            if (errorSource != "")
             {
-
+                errorMessage = DateTime.Now + "\t" + errorInfo + "( from " + errorSource + " )";
             }
-            if (isLogging == true)
+            else
+            {
+                errorMessage = DateTime.Now + "\t" + errorInfo;
+            }
+            if (isUdpEnabled == true)
+            {
+                byte[] sendbytes = Encoding.Unicode.GetBytes(errorMessage);
+                udpClient.Send(sendbytes, sendbytes.Length);
+            }
+            if (isFileEnabled == true)
             {
                 File.AppendAllText(filePath, errorMessage);
             }
+            Console.WriteLine(errorMessage);
             return errorMessage;
         }
+        
     }
     class Program
     {
         static void Main(string[] args)
         {
+            string fileName = "EventList.txt";
+            string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+
+            EventLogger EventLogger = new EventLogger(filePath);
+            EventLogger.start();
+            EventLogger.log("localhost", "Just a Test");
+            Console.ReadLine();
         }
     }
 }
